b106dacc750df4111fb2479ce0f1e0f9dfd7392b
Paez, Andrew <andrewozumba@gmail.com>
2017-08-10 19:42:47 -0400

    Database Options

--- app/src/main/java/com/paezand/database/MainActivity.java
+++ app/src/main/java/com/paezand/database/MainActivity.java
@@ -17,22 +17,30 @@ public class MainActivity extends AppCompatActivity {
 
             SQLiteDatabase myDatabase = this.openOrCreateDatabase("Users", MODE_PRIVATE, null);
 
-            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS users(name VARCHAR, age INT(3))");
+            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS newUsers (name VARCHAR, age INTEGER(3), id INTEGER PRIMARY KEY)");
 
-            myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Rob', 34)");
+//            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS users(name VARCHAR, age INT(3))");
 
-            myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Andres', 32)");
+//            myDatabase.execSQL("INSERT INTO newUsers (name, age) VALUES ('Rob', 34)");
 
+//            myDatabase.execSQL("INSERT INTO newUsers (name, age) VALUES ('Andres', 32)");
+
+//            myDatabase.execSQL("UPDATE users SET age=33 WHERE name = 'Andres'");
+
+//            myDatabase.execSQL("DELETE FROM users WHERE name = 'Andres' LIMIT 1");
+            
             Cursor c = myDatabase.rawQuery("SELECT * FROM users", null);
 
             int nameIndex = c.getColumnIndex("name");
             int ageIndex = c.getColumnIndex("age");
+//            int idIndex = c.getColumnIndex("id");
 
             c.moveToFirst();
 
             while(c != null) {
                 Log.d("name: ", c.getString(nameIndex));
                 Log.d("age: ", Integer.toString(c.getInt(ageIndex)));
+//                Log.d("id: ", Integer.toString(c.getInt(idIndex)));
                 c.moveToNext();
             }
 
