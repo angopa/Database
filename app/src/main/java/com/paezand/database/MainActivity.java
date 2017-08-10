b04876d281455362099ac34178c21c096b197f26
Paez, Andrew <andrewozumba@gmail.com>
2017-08-10 19:01:18 -0400

    Start Repo.

---
+++ app/src/main/java/com/paezand/database/MainActivity.java
@@ -0,0 +1,45 @@
+package com.paezand.database;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try {
+
+            SQLiteDatabase myDatabase = this.openOrCreateDatabase("Users", MODE_PRIVATE, null);
+
+            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS users(name VARCHAR, age INT(3))");
+
+            myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Rob', 34)");
+
+            myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Andres', 32)");
+
+            Cursor c = myDatabase.rawQuery("SELECT * FROM users", null);
+
+            int nameIndex = c.getColumnIndex("name");
+            int ageIndex = c.getColumnIndex("age");
+
+            c.moveToFirst();
+
+            while(c != null) {
+                Log.d("name: ", c.getString(nameIndex));
+                Log.d("age: ", Integer.toString(c.getInt(ageIndex)));
+                c.moveToNext();
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
